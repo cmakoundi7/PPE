e053ff0d3fe6684d1c2d42ccb91df121bec06608
cmakoundi7 <62093446+cmakoundi7@users.noreply.github.com>
2020-03-12 09:00:59 +0100

    INITIAL COMMIT

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer id="footer">
+	<p span class="fond"><b>2019 Copyright Grace's Clothes, Tous droits réservés</b></br>
+	<b>Contact: 0607882968</b>
+	</span>
+	</p>
+</div>
+<div id="reseau">
+	<div id="social">
+	<a href="https://fr-fr.facebook.com"><img src="img/facebook1.png" alt="facebook" /> </br> 
+	<a href="https://www.instagram.com"><img src="img/instagram2.png" alt="instagram" /> </br>
+	<a href="https://twitter.com/twitter"><img src="img/twitter4.jpg" alt="twitter" /> </br>
+	</div>
+	<div id="contact">
+	<a href="https://mail.google.com/mail/u/0/"><img src="img/mail3.png" alt="mail" /> </br> 
+	<a href="https://www.pagesjaunes.fr/pagesblanches"><img src="img/telephone3.png" alt="telephone" /> </br>
+	<a href="https://web.whatsapp.com/"><img src="img/whattsapp.png" alt="whattsapp" /> </br>
+	</div>
+</div>	
+	
+</footer>
